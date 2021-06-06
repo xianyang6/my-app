5ac8acf58ec741ef990d508864b33c47c33a6c20
Owen Chen <owendali12345@gmail.com>
2021-06-06 21:59:24 +0800

    add more img to main

--- src/App.js
+++ src/App.js
@@ -87,7 +87,21 @@ function NextImg() {
   var ImageId = document.getElementById("img-1");
   if (ImageId.src.match("https://i.postimg.cc/DwcFRmnt/DJI-0135.jpg")){
      ImageId.src = "https://i.postimg.cc/TwbXdzzL/963A9831.jpg";
-  } else {
+  } else if (ImageId.src.match("https://i.postimg.cc/TwbXdzzL/963A9831.jpg")){
+    ImageId.src = "https://i.postimg.cc/d3ZCLq2Y/963A9794.jpg";
+  } else if (ImageId.src.match("https://i.postimg.cc/d3ZCLq2Y/963A9794.jpg")){
+    ImageId.src = "https://i.postimg.cc/xTnR6ZS3/963A9797.jpg";
+  } else if (ImageId.src.match("https://i.postimg.cc/xTnR6ZS3/963A9797.jpg")){
+    ImageId.src ="https://i.postimg.cc/63Ts4QMT/963A9811.jpg";
+  } else if (ImageId.src.match("https://i.postimg.cc/63Ts4QMT/963A9811.jpg")) {
+    ImageId.src = "https://i.postimg.cc/QdxrZScp/963A9867.jpg";
+  } else if (ImageId.src.match("https://i.postimg.cc/QdxrZScp/963A9867.jpg")) {
+    ImageId.src = "https://i.postimg.cc/XYTxkYNp/963A9801.jpg";
+  } else if (ImageId.src.match("https://i.postimg.cc/XYTxkYNp/963A9801.jpg")) {
+    ImageId.src = "https://i.postimg.cc/qRfB8Y58/963A9807.jpg";
+  } else if (ImageId.src.match("https://i.postimg.cc/qRfB8Y58/963A9807.jpg")) {
+    ImageId.src = "https://i.postimg.cc/GpCSGxj4/963A9816.jpg";
+  } else if (ImageId.src.match("https://i.postimg.cc/GpCSGxj4/963A9816.jpg")) {
     ImageId.src = "https://i.postimg.cc/DwcFRmnt/DJI-0135.jpg";
   }
 }
