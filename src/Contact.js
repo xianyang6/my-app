5d38bd8fb2a13a608a41ccbe2df3f479ce7e323e
Owen Chen <owendali12345@gmail.com>
2021-06-09 16:12:55 +0800

    push a form

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,11 @@
 import React from 'react';
-
+import styles from './Contact.module.css'
 export default function Contact() {
-    return <p>Contact Page</p>;
+    return (
+    <>
+    <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
+    <iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrk4qY6DJZ6n8EBw?backgroundColor=yellow" frameborder="0" onmousewheel="" className={styles.form}></iframe>
+    <p>Contact Page</p>
+    </>
+    );
 }
