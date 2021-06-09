60e282aa628434b94aa575cfebc6e35b6484f3d9
Owen Chen <owendali12345@gmail.com>
2021-06-09 21:01:24 +0800

    add phone number and img

--- src/Contact.js
+++ src/Contact.js
@@ -9,7 +9,13 @@ export default function Contact() {
     <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
     <iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrk4qY6DJZ6n8EBw?backgroundColor=yellow" title ="a" frameborder="0" onmousewheel="" className={styles.form}></iframe>
     </div> 
-    
+    </div>
+    <div className ={styles.barBox}>
+        <img src ="Contact/phone.png" alt ="" className ={styles.phoneCartoon}>
+        </img>
+      <p className = {styles.phoneNumber}>
+          0510-86355008
+      </p>
     </div>
     </>
     );
