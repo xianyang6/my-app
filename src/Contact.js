6d716ea14473e1078a6aab5fca27910cc9230e49
Owen Chen <owendali12345@gmail.com>
2021-06-09 19:41:21 +0800

    git push

--- src/Contact.js
+++ src/Contact.js
@@ -3,9 +3,14 @@ import styles from './Contact.module.css'
 export default function Contact() {
     return (
     <>
+    <div className ={styles.imgBox}>
+    <img src ="Contact/963A9885.jpg" alt="" className ={styles.img}></img>
+    <div className ={styles.formA}>
     <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
     <iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrk4qY6DJZ6n8EBw?backgroundColor=yellow" title ="a" frameborder="0" onmousewheel="" className={styles.form}></iframe>
-    <p>Contact Page</p>
+    </div> 
+    
+    </div>
     </>
     );
 }
